69a4c6df820bca697dcc5e201f5169f8589cd2c6
Yogesh Suresh Jagdale <yog99mail@gmail.com>
2016-01-11 14:56:14 +0530

    website/index.js deleted
    website/index.js deleted

--- website/index.js
+++
@@ -1,7 +0,0 @@
-//
-var http = require("http");
-http.createServer(function(request, response){
-	response.setHeader("Content-Type", "text/html");
-	//request.on("data")
-	response.end("hello jSymbolic!");
-}).listen(8081);
